e35082f87242fe417c50dc0dea1455915b9968dc
Celestial Phineas <hanghang0713@hotmail.com>
2018-05-19 11:31:17 +0800

    Initialized

--- js/index.js
+++ js/index.js
@@ -1,60 +1,117 @@
-// WebVR Polyfill Configurations
-WebVRConfig = {
-    BUFFER_SCALE: 0.5,
-}
-
+// Initialize polyfill
+var polyfill        = new WebVRPolyfill((function() {
+    var config = {};
+    var q = window.location.search.substring(1);
+    if (q === '') {
+      return config;
+    }
+    var params = q.split('&');
+    var param, name, value;
+    for (var i = 0; i < params.length; i++) {
+      param = params[i].split('=');
+      name = param[0];
+      value = param[1];
+      // All config values are either boolean or float
+      config[name] = value === 'true' ? true :
+                     value === 'false' ? false :
+                     parseFloat(value);
+    }
+    return config;
+})());
 // Three.js renderer
-var renderer
-// Three.js scene
-var scene;
-// Three.js camera object
-var camera;
-// Three.js control object
-var controls;
-// Three.js texture loader
-var textureLoader;
+var renderer        = new THREE.WebGLRenderer({antialias: true, alpha: true, preserveDrawingBuffer: true});
 // Effect
-var effect;
+var effect          = new THREE.VREffect(renderer);
+// Three.js scene
+var scene           = new THREE.Scene();
+// Three.js texture loader
+var textureLoader   = new THREE.TextureLoader();
+// Three.js camera object, initialized in ready
+var camera;
+// Three.js control object, initialized in ready
+var controls;
 // Rendering enter/exit UI
 var vrButton;
 
+var vrDisplay;
+
+var sunLight;
+
 $(document).ready(function () {
-    // Set Three.js renderer
-    renderer = new THREE.WebGLRenderer({antialias: true});
+    // Three.js renderer
     renderer.setPixelRatio(window.devicePixelRatio);
-    // Append the canvas element to the document
     document.body.appendChild(renderer.domElement);
-    // Create a three.js scene
-    scene = new THREE.Scene();
     // Create a three.js camera
     var aspectRatio = window.innerWidth / window.innerHeight;
-    camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 10000);
-    // Controls initializaiton
-    controls = new THREE.VRControls(camera);
-    controls.standing = true;
-    camera.position.y = controls.userHeight;
-    // Window resize call back
-    function onResize() {
-        effect.setSize(window.innerWidth, window.innerHeight);
-        camera.aspect = window.innerWidth/window.innerHeight;
-        camera.updateProjectionMatrix();
-    }
-    $(window).resize(onResize);
-    $(window).bind('vrdisplaypresentchange', onResize);
+    camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 20000);
     // Apply VR stereo rendering to renderer
-    effect = new THREE.VREffect(renderer);
     effect.setSize(window.innerWidth, window.innerHeight);
+    // Initialize WebVR UI
+    initWebVR();
     // Add skybox
     initSkybox();
-    // Initialize WebVR UI
-    initWebVRUI();
+    // Add light
+    initLight();
 });
 
-function initSkybox() {
+// Window resize call back
+function onResize() {
+    effect.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth/window.innerHeight;
+    camera.updateProjectionMatrix();
+}
+$(window).resize(onResize);
+$(window).bind('vrdisplaypresentchange', onResize);
 
+function initSkybox() {
+    var skyboxTextureFilenames = [
+        'res/skybox/posX.jpg', 'res/skybox/negX.jpg',
+        'res/skybox/posY.jpg', 'res/skybox/negY.jpg',
+        'res/skybox/posZ.jpg', 'res/skybox/negZ.jpg'];
+    var materialArray = [];
+    for(var i = 0; i < 6; i++) {
+        (function (i) {
+            textureLoader.load(skyboxTextureFilenames[i], function(texture) {
+                materialArray[i] = new THREE.MeshBasicMaterial({
+                    map:    texture,
+                    side:   THREE.BackSide
+                })
+            });
+        }) (i);
+    }
+    var skyboxGeometry = new THREE.CubeGeometry(5000, 5000, 5000);
+    var skybox = new THREE.Mesh(skyboxGeometry, materialArray);
+    skybox.rotateX(Math.PI/2);
+    scene.add(skybox);
 }
 
-function initWebVRUI() {
+function initLight() {
+    // Add light
+    sunLight = new THREE.PointLight(0xFFFFFF, 1.0);
+    sunLight.position.set(0, 0, 0);
+    scene.add(sunLight);
+}
+
+function initWebVR() {
+    // Controls initializaiton
+    navigator.getVRDisplays().then(function(vrDisplays) {
+        // If we have a native display, or a Cardboard VR Display, then use it
+        if(vrDisplays.length) {
+            vrDisplay = vrDisplays[0];
+            // Apply VR headset positional data to camera
+            controls = new THREE.VRControls(camera);
+            controls.standing = true;
+            // Kick off the render loop
+            vrDisplay.requestAnimationFrame(animate);
+        }
+        // Otherwise we're on a desktop environment with no native
+        // displays, thus provide the controls for a monoscopic view
+        else {
+            controls = new THREE.OrbitControls(camera);
+            controls.target.set(0, 0, -1);
+            requestAnimationFrame(animate);
+        }
+    });
     vrButton = new webvrui.EnterVRButton(renderer.domElement, {
         color:      'black',
         background: 'white',
@@ -76,3 +133,17 @@ function initWebVRUI() {
     });
 }
 
+var lastRender = 0;
+function animate(timestamp) {
+    var delta = Math.min(timestamp - lastRender, 500);
+    lastRender = timestamp;
+    // Update VR headset position and apply to camera.
+    controls.update();
+    // Render the scene.
+    effect.render(scene, camera);
+    if (vrDisplay) {
+        vrDisplay.requestAnimationFrame(animate);
+    } else {
+        requestAnimationFrame(animate);
+    }
+}
